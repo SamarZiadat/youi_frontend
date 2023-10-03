486f6fd44b0eb05180282ff773c8cf1bfdaecf00
SamarZiadat <samarziadat@gmail.com>
2023-10-03 17:27:45 +0000

    build: axios interceptors to adjust refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://youi-backend-25262e22ef8b.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
