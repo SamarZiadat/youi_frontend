9b9d8f3733ff67daeb6d9c55048b00f398bb4d8b
SamarZiadat <samarziadat@gmail.com>
2023-10-04 13:21:00 +0000

    build: capabilities to like posts

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -7,39 +7,46 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
+
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
 
 function PostPage() {
-  const { id } = useParams();
-  const [post, setPost] = useState({ results: [] });
+    const { id } = useParams();
+    const [post, setPost] = useState({ results: [] });
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
 
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const [{ data: post }] = await Promise.all([
-          axiosReq.get(`/posts/${id}`),
-        ]);
-        setPost({ results: [post] });
-        console.log(post);
-      } catch (err) {
-        console.log(err);
-      }
-    };
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const [{ data: post }] = await Promise.all([
+                    axiosReq.get(`/posts/${id}`),
+                    axiosReq.get(`/comments/?post=${id}`),
+                ]);
+                setPost({ results: [post] });
 
-    handleMount();
-  }, [id]);
+            } catch (err) {
+                //console.log(err);
+            }
+        };
 
-  return (
-    <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
-        <p>Post component</p>
-        <Container className={appStyles.Content}>Comments</Container>
-      </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
-      </Col>
-    </Row>
-  );
+        handleMount();
+    }, [id]);
+
+    return (
+        <Row className="h-100">
+            <Col className="py-2 p-0 p-lg-2" lg={8}>
+                Popular profiles mobile
+                <Post {...post.results[0]} setPosts={setPost} postPage />
+                <Container className={appStyles.Content}>Comments</Container>
+            </Col>
+            <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+                Popular Profiles
+            </Col>
+        </Row>
+    );
 }
 
 export default PostPage;
