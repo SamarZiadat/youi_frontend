c174fa0f6aa78ada3a5090db447a9dd681ac5dc0
SamarZiadat <samarziadat@gmail.com>
2023-10-04 14:24:28 +0000

    build: search functionality for posts

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -12,6 +12,7 @@ import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
 import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 import NoResults from "../../assets/no-results.png";
 
@@ -20,25 +21,48 @@ function PostsPage({ message, filter = "" }) {
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
+  const [query, setQuery] = useState("");
+
+  const currentUser = useCurrentUser();
+
   useEffect(() => {
     const fetchPosts = async () => {
-      try {
-        const { data } = await axiosReq.get(`/posts/?${filter}`);
-        setPosts(data);
-        setHasLoaded(true);
-      } catch (err) {
-        console.log(err);
-      }
+        try {
+            const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
+            setPosts(data);
+            setHasLoaded(true);
+        } catch (err) {
+            //console.log(err);
+        }
     };
 
     setHasLoaded(false);
-    fetchPosts();
-  }, [filter, pathname]);
+    const timer = setTimeout(() => {
+        fetchPosts();
+    }, 1000);
+
+    return () => {
+        clearTimeout(timer);
+    };
+}, [filter, query, pathname, currentUser]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+                    className={styles.SearchBar}
+                    onSubmit={(event) => event.preventDefault()}
+                >
+                    <Form.Control
+                        value={query}
+                        onChange={(event) => setQuery(event.target.value)}
+                        type="text"
+                        className="mr-sm-2"
+                        placeholder="Search posts by keywords or tags..."
+                    />
+                </Form>
         {hasLoaded ? (
           <>
             {posts.results.length ? (
