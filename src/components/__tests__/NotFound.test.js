6dcfc5579b2da72c75bf9c4f8b183a1612e5a295
SamarZiadat <samarziadat@gmail.com>
2023-10-20 09:06:46 +0000

    docs: create testing file and basic structure for readme

---
+++ src/components/__tests__/NotFound.test.js
@@ -0,0 +1,20 @@
+// Component imports
+import PageNotFound from "../assets/page-not-found-bg.png";
+import Asset from "./Asset";
+// CSS imports
+import styles from "../styles/NotFound.module.css";
+// React imports
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+const NotFound = () => {
+  return (
+    <>
+      <div className={`${styles.NotFound} mt-3 text-center`}>
+        <Link to={"/"}>404 Error. Click here to return to the Homepage.</Link>
+        <Asset src={PageNotFound} />
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
